a1f301b74669d72ac520a45196cc36ba6c1cad5e
Christos Fitsialis <xfitsialis@gmail.com>
2024-10-06 22:26:34 +0300

    Add db initialization and clearing of tables before every start

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS creditor cascade;
+DROP TABLE IF EXISTS debtor cascade;
+DROP TABLE IF EXISTS invoice cascade;
+DROP TABLE IF EXISTS purchaser cascade;
+DROP TABLE IF EXISTS purchaser_financing_settings cascade;
+DROP TABLE IF EXISTS purchaser_purchaser_financing_settings cascade;
