fcd8be85c7aa44eec489149442935089b2f4080a
Christos Fitsialis <xfitsialis@gmail.com>
2024-10-07 18:39:10 +0300

    Initial commit for unit tests

---
+++ src/test/java/lu/crx/financing/FinancingServiceTest.java
@@ -0,0 +1,13 @@
+package lu.crx.financing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FinancingServiceTest {
+
+    @Test
+    public void test() {
+
+    }
+}
