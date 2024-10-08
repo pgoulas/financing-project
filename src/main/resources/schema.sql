fe45d6da6b65462941a5caabcfbc0a72c6b96827
Christos Fitsialis <xfitsialis@gmail.com>
2024-10-08 19:44:06 +0300

    Clear financing_results table before every run

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,4 @@ DROP TABLE IF EXISTS invoice cascade;
 DROP TABLE IF EXISTS purchaser cascade;
 DROP TABLE IF EXISTS purchaser_financing_settings cascade;
 DROP TABLE IF EXISTS purchaser_purchaser_financing_settings cascade;
+DROP TABLE IF EXISTS financing_result cascade;
