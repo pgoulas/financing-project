90c40b37c86e762e906fde427deadc674ac51406
Christos Fitsialis <xfitsialis@gmail.com>
2024-10-08 21:42:49 +0300

    Remove postgres and restore H2 database. Remove Testcontainers infrastructure.

--- src/test/java/lu/crx/financing/services/FinancingServiceTest.java
+++ src/test/java/lu/crx/financing/services/FinancingServiceTest.java
@@ -1,58 +1,23 @@
 package lu.crx.financing.services;
 
 import lu.crx.financing.repositories.InvoiceRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class FinancingServiceTest {
 
     @Autowired
-    private FinancingService financingService;
+    private InvoiceRepository invoiceRepository;;
 
-    @Autowired
-    private SeedingService seedingService;
-
-    @Autowired
-    private InvoiceRepository invoiceRepository;
-
-    @LocalServerPort
-    private Integer port;
-
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-            "postgres:latest"
-    );
-
-    @BeforeAll
-    static void beforeAll() {
-        postgres.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        postgres.stop();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @BeforeEach
     void setUp() {
-        seedingService.seedMasterData();
+        //seedingService.seedMasterData();
     }
 
     @Test
