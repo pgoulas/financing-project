b7c1f6267224018c148e78fe368f5360622d8955
Christos Fitsialis <xfitsialis@gmail.com>
2024-10-08 19:28:51 +0300

    Add testcontainers structure and simple unit test

--- src/test/java/lu/crx/financing/services/FinancingServiceTest.java
+++ src/test/java/lu/crx/financing/services/FinancingServiceTest.java
@@ -1,17 +1,62 @@
 package lu.crx.financing.services;
 
+import lu.crx.financing.repositories.InvoiceRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
-public class FinancingServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FinancingServiceTest {
+
+    @Autowired
+    private FinancingService financingService;
+
+    @Autowired
+    private SeedingService seedingService;
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:latest"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
+        seedingService.seedMasterData();
     }
 
     @Test
-    public void test() {
+    void testSeedingData() {
+        assertEquals(15, invoiceRepository.findAll().size());
     }
 }
